6ddd03c3152d90c4e91a58f818a57ffef062e3a4
Mike Blackstock <mikeblackstock@gmail.com>
2014-06-02 14:28:57 -0700

    save where I left off so I only send new data from the file

--- simulator/filesender.py
+++ simulator/filesender.py
@@ -16,25 +16,46 @@ import sys
 # 	payload = {'value': float(reading.get('Value'))}
 # 	requests.post("http://wotkit.sensetecnic.com/api/sensors/mike.blood-sensor/data", auth=('b781be7908b3787b', 'f5bde2beb22a0653'), data=payload)
 
-def sendData(readings):
+def sendData(readings, lastTime):
 	'''assumes this is a list of readings with a DisplayTime and Value
 	attribute'''
 	for reading in readings:
 		timestamp_str = reading.get('DisplayTime')
 		dt = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S")
+		
+
 		timestamp=calendar.timegm(dt.utctimetuple())
+		
 		value = float(reading.get('Value'))
 		payload = {'timestamp':timestamp*1000, 'value': float(reading.get('Value'))}
 		print payload
 		# send the data to the wotkit
-		r = requests.post("http://wotkit.sensetecnic.com/api/sensors/mike.blood-sensor/data", auth=('b781be7908b3787b', 'f5bde2beb22a0653'), data=payload)
-		print r.status_code
-		
+		if timestamp > lastTime:
+			print "sending data"
+			r = requests.post("http://wotkit.sensetecnic.com/api/sensors/mike.blood-sensor/data", auth=('b781be7908b3787b', 'f5bde2beb22a0653'), data=payload)
+			print r.status_code
+			f = open('last-time.txt', 'w+')
+			f.write(str(timestamp)+'\n')
+			f.close()	
+		else:
+			print "skipping - old data"
+
+
+
 if __name__ == "__main__":
 	filename = sys.argv[1]
 	tree = ET.parse(filename)
 	root = tree.getroot()
-	sendData(root.find('GlucoseReadings'))
+
+	lastTime = 0
+	try:
+		f = open('last-time.txt', 'r+')
+		lastTime = int(f.readline())
+		f.close()
+	except:
+		pass #do nothing
+
+	sendData(root.find('GlucoseReadings'), lastTime)
 
 
 
